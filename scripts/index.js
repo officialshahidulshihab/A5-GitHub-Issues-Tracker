8c64374231c1ed2b99006d529a016dbc9da86f29
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-03-07 16:28:58 +0600

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const userName=document.getElementById('user-name')
+const userPassword=document.getElementById('user-password')
+const SignButton=document.getElementById('sing-in-btn')
+
+// const createElements= (arr)=>{
+//     const htmlElements=arr.map((el)=> `<span class="btn">${el}</span>`)
+//     return(htmlElements.join(" "));
+// }
+
+SignButton.addEventListener('click', function(){
+    const userNameValue=userName.value
+    // User name 
+    if(userNameValue!=='admin'){
+        alert('User name not found ')
+        return;
+    }
+
+    const userPasswordValue =userPassword.value
+
+    // user Pass
+    if(userPasswordValue!=='admin123'){
+        alert('Wrong Password, Try Again')
+        return;
+
+    }else{
+        alert('Welcome to GitHub Issues Tracker')
+        window.location.href='issues.html'
+        
+
+    }
+    
+    
+     
+
+})
+
